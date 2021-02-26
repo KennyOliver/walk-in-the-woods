97d9d9e9259693e566c5944c689f06dc12e73087
Kenny <kenny_oliver@icloud.com>
2021-02-26 19:40:19 +0000

    [+] Feature that displays adjacent objects, in similar style to the compass

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
 world_map = {
-  51:"T", 52:"N", 53:"N", 54:"N", 55:"T",
-  41:"N", 42:"N", 43:"H", 44:"N", 45:"T",
-  31:"H", 32:"T", 33:"C", 34:"N", 35:"N",
-  21:"N", 22:"T", 23:"T", 24:"N", 25:"N",
-  11:"T", 12:"T", 13:"T", 14:"H", 15:"N"
+  51:"T", 52:"0", 53:"0", 54:"0", 55:"T",
+  41:"0", 42:"0", 43:"H", 44:"0", 45:"T",
+  31:"H", 32:"T", 33:"C", 34:"0", 35:"0",
+  21:"0", 22:"T", 23:"T", 24:"0", 25:"0",
+  11:"T", 12:"T", 13:"T", 14:"H", 15:"0"
 }
 #print(world_map.keys())
 
-print("<-- Walk In The Woods -->")
 compass = "  N\nW + E\n  S"
-print("<-- Info -->\nC = center\nT = tree\nH = house\nN = nothing")
+print("<-- Walk In The Woods -->")
+print("<-- Info -->\nC = center\nT = tree\nH = house\n0 = nothing")
 
 player_location = 33 #initial location (i.e. spawn point)
 
 while True:
-  print("-" * 30)
-  print(compass)
-  try:
+  ''' try:
     print(f"You\'re at:\t{world_map[player_location]}")
   except KeyError:
-    print("YOU\'RE IN THE VOID - GO BACK!")
+    print("YOU\'RE IN THE VOID - GO BACK!") '''
+  
   try:
-    print(f"North is:\t{world_map[player_location + 10]}")
+    display_location = world_map[player_location]
   except KeyError:
-    print("North is:\tVOID!")
+    display_location = "V"
   try:
-    print(f"East is:\t{world_map[player_location + 1]}")
+    north_object = world_map[player_location + 10]
   except KeyError:
-    print("East is:\tVOID!")
+    north_object = "V"
   try:
-    print(f"South is:\t{world_map[player_location - 10]}")
+    east_object = world_map[player_location + 1]
   except KeyError:
-    print("South is:\tVOID!")
+    east_object = "V"
   try:
-    print(f"West is:\t{world_map[player_location - 1]}")
+    south_object = world_map[player_location - 10]
   except KeyError:
-    print("West is:\tVOID!")
+    south_object = "V"
+  try:
+    west_object = world_map[player_location - 1]
+  except KeyError:
+    west_object = "V"
+  #adjacent_objects = f"  {world_map[player_location + 10]}\n{world_map[player_location - 1]} {world_map[player_location]} {world_map[player_location + 1]}\n  {world_map[player_location - 10]}"
+  adjacent_objects = f"  {north_object}\n{west_object} {display_location} {east_object}\n  {south_object}"
+  print("-" * 30)
+  print(compass)
+  print("\n")
+  print(adjacent_objects)
   
   walk_direction = input("Go --> ").upper()
   while walk_direction not in ["N","E","S","W"]:
