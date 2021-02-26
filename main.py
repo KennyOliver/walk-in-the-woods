c3a6156a7f304f0c00190fa35894a6f5ed9d1202
Kenny Oliver <kenny_oliver@icloud.com>
2021-02-26 21:36:33 +0000

    [+] Added mountains to world_map

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 world_map = {
   71:"T", 72:"T", 73:"T", 74:"T", 75:"T", 76:"0", 77:"W",
   61:"T", 62:"0", 63:"T", 64:"0", 65:"T", 66:"T", 67:"W",
-  51:"T", 52:"0", 53:"0", 54:"0", 55:"T", 56:"T", 57:"0",
-  41:"W", 42:"W", 43:"H", 44:"S", 45:"T", 46:"T", 47:"T",
-  31:"W", 32:"W", 33:"W", 34:"0", 35:"0", 36:"T", 37:"H",
+  51:"T", 52:"0", 53:"0", 54:"0", 55:"M", 56:"T", 57:"0",
+  41:"W", 42:"W", 43:"H", 44:"S", 45:"T", 46:"M", 47:"M",
+  31:"W", 32:"W", 33:"W", 34:"0", 35:"T", 36:"M", 37:"H",
   21:"H", 22:"W", 23:"W", 24:"0", 25:"0", 26:"T", 27:"H",
   11:"T", 12:"T", 13:"T", 14:"H", 15:"H", 16:"H", 17:"T"
 }
@@ -11,7 +11,7 @@ world_map = {
 
 compass = "/ N \ \nW + E\n\ S /"
 print("<-- Walk In The Woods -->")
-print("<-- Info -->\nS = spawn point\nT = tree\nW = water\nH = house\n0 = nothing")
+print("<-- Info -->\nS = spawn point\nT = tree\nW = water\nH = house\nM = mountain\n0 = nothing")
 
 player_location = 44 #initial location (i.e. spawn point)
 
