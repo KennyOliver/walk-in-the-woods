c8895bc611754b31bc4471483932b1176db1e472
Kenny <kenny_oliver@icloud.com>
2021-02-26 16:10:56 +0000

    [+] Working!!! The player can walk through a 3x3 world! :happy:

--- main.py
+++ main.py
@@ -5,15 +5,25 @@ world = [
   ]
 print(world)
 
-map = {
-  11: "",
-  12: "",
-  13: "",
-  21: "",
-  22: "",
-  23: "",
-  31: "",
-  32: "",
-  33: ""
+world_map = {
+  31: "7", 32: "8", 33: "9",
+  21: "4", 22: "5", 23: "6",
+  11: "1", 12: "2", 13: "3"
 }
-print(map.keys())
+print(world_map.keys())
+
+print("Proof of Concept!")
+
+player_location = 22
+
+for _ in range(0,3):
+  print(f"You are at: {world_map[player_location]}")
+  walk_direction = input("  N\nW + E\n  S\nEnter direction --> ").upper()
+  if walk_direction == "N":
+    player_location += 10
+  elif walk_direction == "E":
+    player_location += 1
+  elif walk_direction == "S":
+    player_location -= 10
+  elif walk_direction == "W":
+    player_location -= 1
