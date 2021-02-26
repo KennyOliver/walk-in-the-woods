c22a82f0951ae0c616b1d22ff32539fc341eafaa
Kenny <kenny_oliver@icloud.com>
2021-02-26 20:49:37 +0000

    [+] Add water objects [+] Extended map to 7x7

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 world_map = {
-  51:"T", 52:"0", 53:"0", 54:"0", 55:"T",
-  41:"0", 42:"0", 43:"H", 44:"0", 45:"T",
-  31:"H", 32:"T", 33:"C", 34:"0", 35:"0",
-  21:"0", 22:"T", 23:"T", 24:"0", 25:"0",
-  11:"T", 12:"T", 13:"T", 14:"H", 15:"0"
+  71:"T", 72:"T", 73:"T", 74:"T", 75:"T", 76:"0", 77:"W",
+  61:"T", 62:"0", 63:"T", 64:"0", 65:"T", 66:"T", 67:"W",
+  51:"T", 52:"0", 53:"0", 54:"0", 55:"T", 56:"T", 57:"0",
+  41:"W", 42:"W", 43:"H", 44:"S", 45:"T", 46:"T", 47:"T",
+  31:"W", 32:"W", 33:"W", 34:"0", 35:"0", 36:"T", 37:"H",
+  21:"H", 22:"W", 23:"W", 24:"0", 25:"0", 26:"T", 27:"H",
+  11:"T", 12:"T", 13:"T", 14:"H", 15:"H", 16:"H", 17:"T"
 }
 #print(world_map.keys())
 
 compass = "/ N \ \nW + E\n\ S /"
 print("<-- Walk In The Woods -->")
-print("<-- Info -->\nC = center\nT = tree\nH = house\n0 = nothing")
+print("<-- Info -->\nS = spawn point\nT = tree\nW = water\nH = house\n0 = nothing")
 
-player_location = 33 #initial location (i.e. spawn point)
+player_location = 44 #initial location (i.e. spawn point)
 
 while True:
   try:
