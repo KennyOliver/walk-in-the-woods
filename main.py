f51fdbfc15b7c16f542871aea310c45c10580598
Kenny <kenny_oliver@icloud.com>
2021-02-26 16:30:20 +0000

    [+] Used letters to signify objects in the world map (C = center; T = tree; H = house; N = nothing) [+] World map expanded to 5x5

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ world = [
   ["","",""],
   ["","",""]
   ]
-print(world)
+#print(world)
 
 world_map = {
-  31: "7", 32: "8", 33: "9",
-  21: "4", 22: "5", 23: "6",
-  11: "1", 12: "2", 13: "3"
+  51:"T", 52:"N", 53:"N", 54:"N", 55:"T",
+  41:"N", 42:"N", 43:"H", 44:"N", 45:"T",
+  31:"H", 32:"T", 33:"C", 34:"N", 35:"N",
+  21:"N", 22:"T", 23:"X", 24:"N", 25:"N",
+  11:"T", 12:"T", 13:"T", 14:"H", 15:"N"
 }
-print(world_map.keys())
+#print(world_map.keys())
 
 print("Proof of Concept!")
 
-player_location = 22
+compass = "  N\nW + E\n  S"
+player_location = 33 #initial location (i.e. spawn point)
 
-for _ in range(0,3):
+print("Info: C = center; T = tree; H = house; N = nothing")
+
+while True:
+  print(compass)
   print(f"You are at: {world_map[player_location]}")
-  walk_direction = input("  N\nW + E\n  S\nEnter direction --> ").upper()
+  walk_direction = input("Enter direction --> ").upper()
   if walk_direction == "N":
     player_location += 10
   elif walk_direction == "E":
