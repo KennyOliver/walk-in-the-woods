97744f5b3792ae17fc12a9f0a98ec5010e615f85
Kenny <kenny_oliver@icloud.com>
2021-02-27 18:25:37 +0000

    [+] Swap “S” for “$” as “spawn point” symbol [+] Rename “nothing” as “empty” [+] Swap “0” for “.” for “empty” [+] Replace void symbol “V” with “ ” (whitespace)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 world_map = {
-  71:"T", 72:"T", 73:"T", 74:"T", 75:"T", 76:"0", 77:"W",
-  61:"T", 62:"0", 63:"T", 64:"0", 65:"T", 66:"T", 67:"W",
-  51:"T", 52:"0", 53:"0", 54:"0", 55:"M", 56:"T", 57:"0",
-  41:"W", 42:"W", 43:"H", 44:"S", 45:"T", 46:"M", 47:"M",
-  31:"W", 32:"W", 33:"W", 34:"0", 35:"T", 36:"M", 37:"H",
-  21:"H", 22:"W", 23:"W", 24:"0", 25:"0", 26:"T", 27:"H",
+  71:"T", 72:"T", 73:".", 74:"T", 75:"T", 76:".", 77:"W",
+  61:"T", 62:".", 63:"T", 64:".", 65:"T", 66:"T", 67:"W",
+  51:"T", 52:".", 53:".", 54:"T", 55:"M", 56:"T", 57:".",
+  41:"W", 42:"W", 43:"H", 44:"$", 45:"T", 46:"M", 47:"M",
+  31:"W", 32:"W", 33:"W", 34:"H", 35:"T", 36:"M", 37:"H",
+  21:"H", 22:"W", 23:"W", 24:".", 25:".", 26:"T", 27:"H",
   11:"T", 12:"T", 13:"T", 14:"H", 15:"H", 16:"H", 17:"T"
 }
 #print(world_map.keys())
@@ -12,7 +12,7 @@ world_map = {
 compass = "/ N \ \nW + E\n\ S /"
 print("<-- Walk In The Woods -->")
 print("A 7x7 world made from a dictionary!")
-print("<-- Info -->\nS = spawn point\nT = tree\nW = water\nH = house\nM = mountain\n0 = nothing")
+print("<-- Info -->\n$ = spawn point\nT = tree\nW = water\nH = house\nM = mountain\n. = empty")
 
 player_location = 44 #initial location (i.e. spawn point)
 
@@ -20,11 +20,11 @@ print("-" * 20)
 print(compass)
 
 while True:
-  display_location = world_map.get(player_location, "V")
-  north_object = world_map.get(player_location + 10, "V")
-  east_object = world_map.get(player_location + 1, "V")
-  south_object = world_map.get(player_location - 10, "V")
-  west_object = world_map.get(player_location - 1, "V")
+  display_location = world_map.get(player_location, " ")
+  north_object = world_map.get(player_location + 10, " ")
+  east_object = world_map.get(player_location + 1, " ")
+  south_object = world_map.get(player_location - 10, " ")
+  west_object = world_map.get(player_location - 1, " ")
   
   adjacent_objects = f"* {north_object} *\n{west_object} {display_location} {east_object}\n* {south_object} *"
   print("-" * 20)
@@ -44,10 +44,9 @@ while True:
   elif walk_direction == "W":
     player_location -= 1
   
-  #print("\033[A\033[A" * 3)
   print("\033[A                             \033[A") #clear previous lines
-  print("\033[A                             \033[A") #space necessary to remove previous input from console
+  print("\033[A                             \033[A") #need space to remove previous console input
   print("\033[A                             \033[A")
   print("\033[A                             \033[A")
   print("\033[A                             \033[A")
-  #print("\033[A                             \033[A")
+  #print("\033[A\033[A" * 3)
