98b7ad89a9a244c15cf05413d9301b5875e5fe64
Kenny <kenny_oliver@icloud.com>
2021-02-28 13:37:20 +0000

    [+] Removed all empty objects, replacing them with other objects; to prevent confusion with void

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 world_map = {
-  71:"T", 72:"T", 73:".", 74:"T", 75:"T", 76:".", 77:"W",
-  61:"T", 62:".", 63:"T", 64:".", 65:"T", 66:"T", 67:"W",
-  51:"T", 52:".", 53:".", 54:"T", 55:"M", 56:"T", 57:".",
+  71:"T", 72:"T", 73:"H", 74:"T", 75:"T", 76:"W", 77:"W",
+  61:"T", 62:"T", 63:"T", 64:"H", 65:"T", 66:"T", 67:"W",
+  51:"T", 52:"T", 53:"W", 54:"T", 55:"M", 56:"T", 57:"W",
   41:"W", 42:"W", 43:"H", 44:"$", 45:"T", 46:"M", 47:"M",
   31:"W", 32:"W", 33:"W", 34:"H", 35:"T", 36:"M", 37:"H",
-  21:"H", 22:"W", 23:"W", 24:".", 25:".", 26:"T", 27:"H",
+  21:"H", 22:"W", 23:"W", 24:"W", 25:"H", 26:"T", 27:"H",
   11:"T", 12:"T", 13:"T", 14:"H", 15:"H", 16:"H", 17:"T"
 }
 #print(world_map.keys())
@@ -12,7 +12,7 @@ world_map = {
 compass = "/ N \ \nW + E\n\ S /"
 print("<-- Walk In The Woods -->")
 print("A 7x7 world made from a dictionary!")
-print("<-- Info -->\n$ = spawn point\nT = tree\nW = water\nH = house\nM = mountain\n. = empty")
+print("<-- Info -->\n$ = spawn point\nT = tree\nW = water\nH = house\nM = mountain")
 
 player_location = 44 #initial location (i.e. spawn point)
 
